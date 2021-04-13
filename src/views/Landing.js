3ddbc60d2e32fa9eb95808ed68e772cf87502cbc
sheknowshowtocode <74694750+sheknowshowtocode@users.noreply.github.com>
2021-04-13 01:32:21 -0400

    BUSK-1 - add EventList on the Landing page (#2)

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,9 +1,11 @@
+import EventList from '../components/EventsList';
 
 function Landing() {
   return (
     <div>
       <h1>Busker</h1>
       <button>Login as a musician</button>
+      <EventList />
     </div>
   )
 }
